56fbfb2e08b68059e199521c79423d770d95fb9c
Pavle <pavleg.19@gmail.com>
2022-04-20 22:23:18 +0200

    Add sample charts

--- MiniProjekat/MainWindow.xaml.cs
+++ MiniProjekat/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,17 @@ namespace MiniProjekat
     /// </summary>
     public partial class MainWindow : Window
     {
+
+        public SeriesCollection LineSeriesCollection { get; set; }
+        public SeriesCollection ColumnSeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+        public Func<double, string> Formatter { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
+            
         }
 
         private void DrawButton_Click(object sender, RoutedEventArgs e)
@@ -41,7 +51,71 @@ namespace MiniProjekat
                 Console.Out.WriteLine(startDate + " " + endDate);
 
                 // fill charts
+                DrawCharts();
+                DrawLineChart();
             }
+        }
+
+        private void DrawCharts()
+        {
+            DrawLineChart();
+            DrawColumnChart();
+            DataContext = this;
+        }
+
+        private void DrawColumnChart()
+        {
+            SolidColorBrush brush = (SolidColorBrush)new BrushConverter().ConvertFrom("#5a7bfb");
+
+            ColumnSeriesCollection = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Title = "2015",
+                    Values = new ChartValues<double> { 10, 50, 39, 50, 37, 28, 15 },
+                    Stroke = brush,
+                    Fill = brush
+                }
+            };
+          
+            //ColumnSeriesCollection[1].Values.Add(48d);
+
+            Labels = new[] { "Maria", "Susan", "Charles", "Frida" };
+            Formatter = value => value.ToString("N");
+        }
+
+        private void DrawLineChart()
+        {
+            SolidColorBrush brush = (SolidColorBrush)new BrushConverter().ConvertFrom("#5a7bfb");
+
+            LineSeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 3",
+                    Values = new ChartValues<double> { 4,2,7,2,7 },
+                    PointGeometry = DefaultGeometries.Square,
+                    PointGeometrySize = 15,
+                    Stroke = brush
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            /*
+            LineSeriesCollection.Add(new LineSeries
+            {
+                Title = "Series 4",
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0, //0: straight lines, 1: really smooth lines
+                PointGeometry = Geometry.Parse("m 25 70.36218 20 -28 -20 22 -8 -6 z"),
+                PointGeometrySize = 50,
+                PointForeground = Brushes.Gray
+            }); 
+            */
+
+            // LineSeriesCollection[3].Values.Add(5d);
         }
 
         private void ClearButton_Click(object sender, RoutedEventArgs e)
